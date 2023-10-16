f44d8ae9eee56faaecaaa094b2736093c9d5a5ac
DavidReinon <davidreinongarcia@gmail.com>
2023-10-16 18:16:32 +0200

    template final

--- App.tsx
+++ App.tsx
@@ -1,41 +1,21 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React, {useState} from 'react';
-import {ScrollView, StatusBar, StyleSheet, Text, View} from 'react-native';
-
-import {Button, TextInput} from 'react-native-paper';
+import React from 'react';
+import {
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+} from 'react-native';
 
 const App = () => {
-  const [text, setText] = useState('');
   return (
     <View>
-      <Text style={styles.sectionTitle}>Texto Inicial</Text>
-      <TextInput
-        label="Email"
-        value={text}
-        onChangeText={element => setText(element)}
-      />
-      <Button mode="text" icon="alien">
-        1
-      </Button>
-      <Button mode="outlined" icon="alien">
-        2
-      </Button>
-      <Button mode="contained" icon="alien">
-        3
-      </Button>
-      <Button mode="elevated" icon="alien">
-        4
-      </Button>
+      <Text style={styles.sectionTitle}>Texto</Text>
     </View>
   );
 };
-
 const styles = StyleSheet.create({
   sectionContainer: {
     marginTop: 32,
@@ -54,5 +34,4 @@ const styles = StyleSheet.create({
     fontWeight: '700',
   },
 });
-
 export default App;
