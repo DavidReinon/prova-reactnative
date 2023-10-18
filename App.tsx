0d934fe1981a7b0deb479501659925dde7ffc372
DavidReinon <davidreinongarcia@gmail.com>
2023-10-18 20:14:06 +0200

    nueva rama

--- App.tsx
+++ App.tsx
@@ -11,27 +11,55 @@ import {
 
 const App = () => {
   return (
-    <View>
-      <Text style={styles.sectionTitle}>Texto</Text>
+    <View style={styles.contenidor}>
+      <View style={styles.seccio1}>
+        <Text>Secció 1</Text>
+      </View>
+      <View style={styles.seccio2}>
+        <Text>Secció 2</Text>
+      </View>
+      <View style={styles.seccio3}>
+        <Text>Secció 3</Text>
+      </View>
     </View>
   );
 };
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  contenidor: {
+    flex: 1,
+    flexDirection: 'row',
+    borderColor: 'purple',
+    borderWidth: 5,
   },
-  sectionTitle: {
-    fontSize: 24,
+  seccio1: {
+    flex: 1,
+    borderColor: 'red',
+    borderWidth: 3,
+    fontSize: 12,
     fontWeight: '600',
+    margin: 4,
+    padding: 12,
+    textAlign: 'right',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  seccio2: {
+    flex: 1,
+    borderColor: 'green',
+    borderWidth: 3,
+    fontSize: 12,
+    fontWeight: '600',
+    margin: 4,
+    padding: 12,
+    textAlign: 'center',
   },
-  highlight: {
-    fontWeight: '700',
+  seccio3: {
+    flex: 1,
+    borderColor: 'blue',
+    borderWidth: 3,
+    fontSize: 12,
+    fontWeight: '600',
+    margin: 4,
+    padding: 12,
+    textAlign: 'left',
   },
 });
 export default App;
