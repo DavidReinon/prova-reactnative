b818c06025ac01cffdc5651fae83f574df09f423
DavidReinon <davidreinongarcia@gmail.com>
2023-10-30 20:39:06 +0100

    aplicacion inicial

--- App.tsx
+++ App.tsx
@@ -1,21 +1,45 @@
 import React from 'react';
-import {
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  View,
-  TextInput,
-  Button,
-} from 'react-native';
+import {useState} from 'react';
+import {ScrollView, StatusBar, StyleSheet, Text, View} from 'react-native';
+import {Button, TextInput} from 'react-native-paper';
+import CalcularIMG from './components/CalcularIMG';
 
 const App = () => {
+  const [pes, setPes] = useState(0);
+  const [alçada, setAlçada] = useState(0);
+  const [desabilitarRespuesta, setDesabilitarRespuesta] = useState(true);
+
   return (
-    <View>
-      <Text style={styles.sectionTitle}>Texto</Text>
+    <View style={styles.sectionContainer}>
+      <Text style={styles.sectionTitle}>Calcular IMC</Text>
+      <TextInput
+        style={styles.textInput}
+        defaultValue=""
+        onChangeText={newText => setPes(parseInt(newText))}
+        placeholder="Pes"
+        // onPointerCancelCapture={() => setDesabilitarRespuesta(true)}
+      />
+      <TextInput
+        style={styles.textInput}
+        defaultValue=""
+        onChangeText={newText => setAlçada(parseInt(newText))}
+        placeholder="Alçada"
+        // onPointerCancelCapture={() => setDesabilitarRespuesta(true)}
+      />
+      <Button
+        icon="alien"
+        mode="contained"
+        onPress={() => setDesabilitarRespuesta(false)}>
+        Calcula
+      </Button>
+      <CalcularIMG
+        disabled={desabilitarRespuesta}
+        pes={pes}
+        alçada={alçada}></CalcularIMG>
     </View>
   );
 };
+
 const styles = StyleSheet.create({
   sectionContainer: {
     marginTop: 32,
@@ -24,11 +48,15 @@ const styles = StyleSheet.create({
   sectionTitle: {
     fontSize: 24,
     fontWeight: '600',
+    alignSelf: 'center',
   },
   sectionDescription: {
     marginTop: 8,
     fontSize: 18,
     fontWeight: '400',
+  },
+  textInput: {
+    margin: 20,
   },
   highlight: {
     fontWeight: '700',
---
+++ components/CalcularIMG.tsx
@@ -0,0 +1,73 @@
+import React, {useState} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const operacion = (pes: number, alçada: number) => {
+  // IMC = Peso (kg) / altura (m)2
+  return pes / Math.pow(alçada / 100, 2);
+};
+
+const messatge = (resultat: number) => {
+  let message = '';
+  switch (true) {
+    case resultat < 18.5:
+      message = 'Peso insuficiente';
+      break;
+    case resultat >= 18.5 && resultat < 25:
+      message = 'Normopeso';
+      break;
+    case resultat >= 25 && resultat < 27:
+      message = 'Sobrepeso grado I (preobesidad)';
+      break;
+    case resultat >= 27 && resultat < 30:
+      message = 'Sobrepeso grado II (preobesidad)';
+      break;
+    case resultat >= 30 && resultat < 35:
+      message = 'Obesidad de tipo I';
+      break;
+    case resultat >= 35 && resultat < 40:
+      message = 'Obesidad de tipo II (mórbida)';
+      break;
+    case resultat >= 40 && resultat < 50:
+      message = 'Obesidad de tipo III (mórbida)';
+      break;
+    default:
+      message = 'Obesidad de tipo IV (extrema)';
+      break;
+  }
+  return message;
+};
+
+interface Props {
+  pes: number;
+  alçada: number;
+  disabled: boolean;
+}
+
+const calcularIMG = (props: Props) => {
+  const [colorInfo, setColorInfo] = useState('red');
+  const result = operacion(props.pes, props.alçada);
+  const message = messatge(result);
+  if (result < 27) setColorInfo('green');
+  if (result > 27 && result < 40) setColorInfo('yellow');
+
+  if (props.disabled) return <View></View>;
+  return (
+    <View style={styles.textContainer}>
+      <Text style={styles.textAlone}>
+        El teu índex de massa corporal és: {result.toFixed(2)}
+      </Text>
+      <Text style={[styles.textAlone, {color: colorInfo}]}>{message}</Text>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  textContainer: {
+    alignSelf: 'center',
+    margin: 10,
+  },
+  textAlone: {
+    fontSize: 19,
+    alignSelf: 'center',
+  },
+});
+export default calcularIMG;
