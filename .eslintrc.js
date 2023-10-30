78383059973bf5564e20a1eac5fc4b133460bb3a
DavidReinon <davidreinongarcia@gmail.com>
2023-10-30 18:56:46 +0100

    cambios formateador

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,8 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: ['plugin:prettier/recommended'],
+  rules: {
+    'arrow-body-style': 'off',
+    'prefer-arrow-callback': 'warn',
+  },
 };
